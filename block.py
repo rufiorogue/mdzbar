a95dc364b7de058d7b142d23e9d738ef55a08c6a
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2016-10-16 04:51:18 +0300

    blinking

--- block.py
+++ block.py
@@ -7,25 +7,48 @@ from mdzbar.eventhook import EventHook
 __all__ = ['Block']
 
 class Block:
+
+    ''' Default padding '''
+    default_padding=(0,0)
+
+    ''' Default update interval '''
+    default_update_interval=2
+
+    ''' Background color attribute used with blink effect '''
+    blink_bg='red'
+
+    ''' Blink interval in seconds '''
+    blink_interval=0.5
+
     '''
     @variable align:left,center,right   Alignment of the block within the bar
 
-    @variable padding:(left,right)      Space between text and block edge.
-                                        NOTE: space between adjacent blocks should
+    @variable padding:(left,right)      Space in chars between text and block edge.
+                                        -NOTE1- padding cannot be specified in pixels
+                                        because dzen2 applies color attributes
+                                        only to text content.
+                                        -NOTE2- space between adjacent blocks should
                                         be modeled with a spacer block instead.
 
     @variable update_interval:float     Update interval in seconds. If zero,
                                         then updated only once
     '''
-    def __init__(self, bg=None, fg=None, align='right', padding=(0,0),
-                 update_interval=2):
+    def __init__(self, bg=None, fg=None, align='right', padding=None,
+                 update_interval=None):
         self._message = ''
         self._bg = bg
         self._fg = fg
         self._align = align
-        self._padding = padding
-        self._interval = update_interval
+        self._padding = padding if padding is not None else Block.default_padding
+        # blinking enabled
+        self._blink = False
+        # blinking state: False=normal bg, True=blink bg
+        self._blinkingstate = False
+        # controls blinking state toggle on and off
+        self._blinkingtimer = None
         self._active = True
+        self._interval = update_interval if update_interval is not None \
+                                         else Block.default_update_interval
         self._timer = None
         self.on_changed = EventHook()
 
@@ -46,12 +69,40 @@ class Block:
         self._active = False
         if self._timer:
             self._timer.cancel()
+        if self._blinkingtimer:
+            self._blinkingtimer.cancel()
+
+    '''
+    Enables/disables blinking effect: background is flashed red each blink_interval
+    '''
+    def set_blink(self, on):
+        if self._blink != on:
+            if on:
+                self._create_blink_timer()
+            else:
+                self._blinkingstate = False
+                self._blinkingtimer.cancel()
+                self._blinkingtimer = None
+            self._blink = on
+
+    def _create_blink_timer(self):
+        self._blinkingtimer = threading.Timer(Block.blink_interval,
+                                functools.partial(Block._on_blink_timer, self))
+        self._blinkingtimer.start()
+
+    def _on_blink_timer(self):
+        self._blinkingstate = not self._blinkingstate
+        self._create_blink_timer()
+        # TODO: optimize: only bg attribute needs to be updated here
+        # therefore we don't need to run the whole thing.
+        self._process()
 
 
     def _process(self):
         content = ''
-        if self._bg:
-            content += '^bg(' + self._bg + ')'
+        bg = Block.blink_bg if self._blinkingstate else self._bg
+        if bg:
+            content += '^bg(' + bg + ')'
         if self._fg:
             content += '^fg(' + self._fg + ')'
         for i in range(0, self._padding[0]):
@@ -62,7 +113,7 @@ class Block:
 
         for i in range(0, self._padding[1]):
             content += ' '
-        if self._bg:
+        if bg:
             content += '^bg()'
         if self._fg:
             content += '^fg()'
--- test/demo_statusbar.py
+++ test/demo_statusbar.py
@@ -12,25 +12,44 @@ sys.path.append('/home/aldn/pr/sw')
 
 
 from mdzbar.bar import Bar
+from mdzbar.block import Block
 from mdzbar.blocks.clock import Clock
 from mdzbar.blocks.statictext import StaticText
 from mdzbar.blocks.separator import Separator
 
+#
 # create the bar
-bar = Bar(bg='#333333',font='Mono-10')
+#
+bar = Bar(bg='#333333',font='Terminus-10')
 
+#
 # create blocks
-clock = Clock(fg='blue', bg='white', padding=(2,0), fmt='%H:%M:%S')
-statictext = StaticText(fg='red', bg='black', padding=(1,1), text='static text')
-separator = Separator(size=10)
+#
 
+Block.default_padding = (1,1)
+Block.default_update_interval = 3
+
+clock = Clock(fg='cyan', bg='black', padding=(2,0), fmt='%H:%M:%S')
+
+text = StaticText(bg='white', fg='#222222', text='hello!')
+
+blinking_text = StaticText(fg='white', text='blinking')
+blinking_text.set_blink(True)
+
+separator = Separator(size=20)
+
+#
 # pack the blocks in the bar
-bar.add_block(statictext)
+#
+bar.add_block(blinking_text)
+bar.add_block(text)
 bar.add_block(separator)
 bar.add_block(clock)
 
+
 # main loop
 bar.run()
+
 
 print("Exited")
 
